08c539f20b734a81eea95598f5156a6ef323348c
Xylitol <jam.xylitol@gmail.com>
2026-04-03 14:12:47 +0800

    feat: enhance Rating component with min/max props

--- frontend/src/lib/components/common/display/Rating.svelte
+++ frontend/src/lib/components/common/display/Rating.svelte
@@ -2,6 +2,10 @@
   export type RatingProps = {
     /** The rating score to display. */
     score?: number | null;
+    /** The minimum value of the rating. */
+    min?: number;
+    /** The maximum value of the rating. */
+    max?: number;
     /** The number of decimal places to show. */
     decimals?: number;
     /** Whether to apply a stroke effect to the text. */
@@ -12,14 +16,27 @@
 </script>
 
 <script lang="ts">
-  let { score, decimals = 1, stroke = true, class: _class }: RatingProps = $props();
+  let { score: _score, min = 0, max = 10, decimals = 1, stroke = true, class: _class }: RatingProps = $props();
+
+  let score: string = $derived.by(() => {
+    if (_score === null || _score === undefined) {
+      return '';
+    }
+    if (_score <= min) {
+      return min.toString();
+    }
+    if (_score >= max) {
+      return max.toString();
+    }
+    return Number(_score).toFixed(decimals);
+  });
 </script>
 
-{#if score !== null && score !== undefined}
+{#if score}
   <span
     class:text-stroke={stroke}
     class="flex-center rounded-selector bg-black/60 px-2 text-yellow-500 opacity-80 {_class}"
   >
-    {Number(score).toFixed(decimals)}
+    {score}
   </span>
 {/if}
